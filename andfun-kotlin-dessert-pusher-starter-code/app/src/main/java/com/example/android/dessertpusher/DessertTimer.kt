e154b8ffefa43b839b0da8f3383d519a0b43227c
Vitor Castro <vitorhugocastro95@gmail.com>
2019-12-09 22:51:43 +0000

    [Dessert Pusher] Lifecycle Observation

--- andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
+++ andfun-kotlin-dessert-pusher-starter-code/app/src/main/java/com/example/android/dessertpusher/DessertTimer.kt
@@ -17,6 +17,9 @@
 package com.example.android.dessertpusher
 
 import android.os.Handler
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 import timber.log.Timber
 
 /**
@@ -34,8 +37,10 @@ import timber.log.Timber
  * https://developer.android.com/guide/components/processes-and-threads
  *
  */
-class DessertTimer {
-
+class DessertTimer(lifecycle: Lifecycle) : LifecycleObserver {
+    init {
+        lifecycle.addObserver(this)
+    }
     // The number of seconds counted since the timer started
     var secondsCount = 0
 
@@ -46,7 +51,7 @@ class DessertTimer {
     private var handler = Handler()
     private lateinit var runnable: Runnable
 
-
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun startTimer() {
         // Create the runnable action, which prints out a log and increments the seconds counter
         runnable = Runnable {
@@ -65,6 +70,7 @@ class DessertTimer {
         // In this case, no looper is defined, and it defaults to the main or UI thread.
     }
 
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun stopTimer() {
         // Removes all pending posts of runnable from the handler's queue, effectively stopping the
         // timer
