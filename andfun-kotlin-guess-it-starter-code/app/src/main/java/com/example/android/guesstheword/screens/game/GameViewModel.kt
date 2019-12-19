bee7e9804d852b35f88d7088ab838277f953a215
Vitor Castro <vitorhugocastro95@gmail.com>
2019-12-19 17:02:56 +0000

    [Guess It] Add LiveData to GameViewModel

--- andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ andfun-kotlin-guess-it-starter-code/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -17,15 +17,16 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel(){
 
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -34,6 +35,8 @@ class GameViewModel : ViewModel(){
         Log.i("GameViewModel", "GameViewModel created!")
         resetList()
         nextWord()
+        score.value = 0
+        word.value = ""
     }
 
     override fun onCleared() {
@@ -79,19 +82,19 @@ class GameViewModel : ViewModel(){
         if (wordList.isEmpty()) {
 //            gameFinished()
         } else {
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
     fun onSkip() {
-        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
     fun onCorrect() {
-        score++
+        score.value = (score.value)?.plus(1)
         nextWord()
     }
 }
