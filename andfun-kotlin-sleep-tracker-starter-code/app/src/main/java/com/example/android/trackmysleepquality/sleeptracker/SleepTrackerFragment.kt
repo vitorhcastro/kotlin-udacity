43a5bed12a97439a85040a1bb4049c9124eb9bdf
Vitor Castro <vitorhugocastro95@gmail.com>
2019-12-24 02:12:10 +0000

    [Sleep Tracker] Change LinearLayout to GridLayout

--- andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
+++ andfun-kotlin-sleep-tracker-starter-code/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerFragment.kt
@@ -25,6 +25,7 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
 import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.database.SleepDatabase
 import com.example.android.trackmysleepquality.databinding.FragmentSleepTrackerBinding
@@ -58,6 +59,9 @@ class SleepTrackerFragment : Fragment() {
         val sleepNightAdapter = SleepNightAdapter()
 
         binding.sleepList.adapter = sleepNightAdapter
+
+        val manager = GridLayoutManager(activity, 3)
+        binding.sleepList.layoutManager = manager
 
         viewModel.nights.observe(viewLifecycleOwner, Observer {
             it?.let {
