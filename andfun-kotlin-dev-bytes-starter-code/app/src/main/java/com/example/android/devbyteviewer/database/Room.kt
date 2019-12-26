955577d97d48e0c26696a9de87ef114552a7011a
Vitor Castro <vitorhugocastro95@gmail.com>
2019-12-26 17:16:38 +0000

    [Dev Bytes] Build a Repository

--- andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
+++ andfun-kotlin-dev-bytes-starter-code/app/src/main/java/com/example/android/devbyteviewer/database/Room.kt
@@ -31,13 +31,15 @@ interface VideoDao {
 }
 
 @Database(entities = [DatabaseVideo::class], version = 1)
-abstract class VideosDatabase: RoomDatabase()
+abstract class VideosDatabase : RoomDatabase() {
+    abstract val videoDao: VideoDao
+}
 
 private lateinit var INSTANCE: VideosDatabase
 
-fun getDatabase(context: Context): VideosDatabase{
-    synchronized(VideosDatabase::class.java){
-        if(!::INSTANCE.isInitialized){
+fun getDatabase(context: Context): VideosDatabase {
+    synchronized(VideosDatabase::class.java) {
+        if (!::INSTANCE.isInitialized) {
             INSTANCE = Room.databaseBuilder(
                     context.applicationContext,
                     VideosDatabase::class.java,
